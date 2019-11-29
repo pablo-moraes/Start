610e150b7a98c9b80b73a140e65d180e7c4800e2
Pablo Moraes <pablomoraes2017@gmail.com>
2019-11-29 19:03:57 -0300

    correções do banco

--- bdcolab.sql
+++ bdcolab.sql
@@ -10,50 +10,81 @@ CREATE TABLE tb_organizacao (
     tp_causa_social VARCHAR(45) NOT NULL, 
     dt_criacao DATE NOT NULL, 
     dt_cadastro DATE NOT NULL,
-    descricao_atividade VARCHAR(255) NOT NULL,
+    ds_atividade VARCHAR(255) NOT NULL,
 	PRIMARY KEY(id_org),
     FOREIGN KEY (id_responsavel_org) REFERENCES tb_no_responsavel(id_responsavel_org)
 );
 
 CREATE TABLE tb_no_responsavel (
 	id_responsavel_org INT(6) NOT NULL AUTO_INCREMENT,
-    nome_responsavel VARCHAR(55) NOT NULL,
+    no_responsavel VARCHAR(55) NOT NULL,
     num_cpf VARCHAR(11) NOT NULL,
     num_rg VARCHAR(11) NOT NULL,
     PRIMARY KEY(id_responsavel_org)
 );
 
-CREATE TABLE tb_telefone_org (
-	id_org INT,
-    tel_org VARCHAR(14),
-    FOREIGN KEY(id_org) REFERENCES tb_organizacao(id_org)
+
+CREATE TABLE tb_telefone(
+	id_telefone INT AUTO_INCREMENT,
+	num_telefone VARCHAR(14),
+    PRIMARY KEY(id_telefone)
 );
 
-CREATE TABLE tb_endereco_org(
-	id_org INT,
-	endereco VARCHAR(20),
-    bairro VARCHAR(20),
-    cep CHAR(8),
-    cidade VARCHAR(30),
-    uf CHAR(2),
-	FOREIGN KEY(id_org) REFERENCES tb_organizacao(id_org)
+CREATE TABLE telefone_organizacao(
+	id_telefone_organizacao INT AUTO_INCREMENT,
+    id_telefone INT,
+    id_org INT,
+	FOREIGN KEY(id_telefone) REFERENCES tb_telefone(id_telefone),
+    FOREIGN KEY(id_org) REFERENCES tb_organizacao(id_org),
+    PRIMARY KEY(id_telefone_organizacao)
 );
 
-CREATE TABLE tb_pessoa_fs (
+CREATE TABLE telefone_pessoa_fisica(
+	id_telefone_pessoa_fs INT AUTO_INCREMENT,
+    id_telefone INT,
+    id_pessoa_fs INT,
+	FOREIGN KEY(id_telefone) REFERENCES tb_telefone(id_telefone),
+    FOREIGN KEY(id_pessoa_fs) REFERENCES tb_pessoa_fisica(id_pessoa_fs),
+    PRIMARY KEY(id_telefone_pessoa_fs)
+);
+
+CREATE TABLE tb_endereco (
+	id_endereco INT AUTO_INCREMENT,
+	ds_endereco VARCHAR(20),
+    ds_bairro VARCHAR(20),
+    ds_cep CHAR(8),
+    ds_cidade VARCHAR(30),
+    ds_uf CHAR(2),
+    PRIMARY KEY(id_endereco)
+);
+
+
+CREATE TABLE endereco_organizacao (
+	id_endereco_organizacao INT AUTO_INCREMENT,
+    id_endereco INT,
+    id_org INT,
+	FOREIGN KEY(id_endereco) REFERENCES tb_endereco(id_endereco),
+    FOREIGN KEY(id_org) REFERENCES tb_organizacao(id_org),
+    PRIMARY KEY(id_endereco_organizacao)
+);
+
+CREATE TABLE endereco_pessoa_fisica(
+	id_endereco_pessoa_fs INT AUTO_INCREMENT,
+    id_endereco INT,
+    id_pessoa_fs INT,
+	FOREIGN KEY(id_endereco) REFERENCES tb_endereco(id_endereco),
+    FOREIGN KEY(id_pessoa_fs) REFERENCES tb_pessoa_fisica(id_pessoa_fs),
+    PRIMARY KEY(id_endereco_pessoa_fs)
+);
+
+CREATE TABLE tb_pessoa_fisica (
 	id_pessoa_fs INT(6) NOT NULL AUTO_INCREMENT,
-    nome_pessoa VARCHAR(55) NOT NULL,
+    no_pessoa VARCHAR(55) NOT NULL,
     num_cpf_pessoa VARCHAR(11) NOT NULL,
     num_rg_pessoa VARCHAR(14),
-	endereco VARCHAR(20),
-    bairro VARCHAR(20),
-    cep CHAR(8),
-    cidade VARCHAR(30),
-    uf CHAR(2),
+	ds_endereco VARCHAR(20),
+    ds_bairro VARCHAR(20),
+    ds_cep CHAR(8),
+    ds_cidade VARCHAR(30),
     PRIMARY KEY(id_pessoa_fs)
-);
-
-CREATE TABLE tb_telefone_pessoa (
-	id_pessoa_fs INT,
-    tel_pessoa VARCHAR(14),
-    FOREIGN KEY (id_pessoa_fs) REFERENCES tb_pessoa_fs(id_pessoa_fs)
 );
